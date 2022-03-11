97b96a41d2ac45938e7664365b6420bf57520eb5
AdmerDev <sjkrantz@aol.com>
2022-03-11 16:18:35 -0600

    Created ArchivedProjects.

--- TheBugTracker/Controllers/ProjectsController.cs
+++ TheBugTracker/Controllers/ProjectsController.cs
@@ -76,6 +76,15 @@ namespace TheBugTracker.Controllers
             return View(projects);
         }
 
+        public async Task<IActionResult> ArchivedProjects()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            List<Project> projects = await _projectService.GetArchivedProjectsByCompany(companyId);
+
+            return View(projects);
+        }
+
         // GET: Projects/Details/5
         public async Task<IActionResult> Details(int? id)
         {
--- TheBugTracker/Services/BTProjectService.cs
+++ TheBugTracker/Services/BTProjectService.cs
@@ -129,7 +129,9 @@ namespace TheBugTracker.Services
         {
             List<Project> projects = new();
 
-            projects = await _context.Projects.Where(p => p.CompanyId == companyId && p.Archived == false)
+            try
+            {
+                projects = await _context.Projects.Where(p => p.CompanyId == companyId && p.Archived == false)
                                             .Include(p => p.Members)
                                             .Include(p => p.Tickets)
                                                 .ThenInclude(t => t.Comments)
@@ -152,7 +154,12 @@ namespace TheBugTracker.Services
                                             .Include(p => p.ProjectPriority)
                                             .ToListAsync();
 
-            return projects;
+                return projects;
+            }
+            catch (Exception)
+            {
+                throw;
+            }            
         }
 
         public async Task<List<Project>> GetAllProjectsByPriority(int companyId, string priorityName)
@@ -165,9 +172,37 @@ namespace TheBugTracker.Services
 
         public async Task<List<Project>> GetArchivedProjectsByCompany(int companyId)
         {
-            List<Project> projects = await GetAllProjectsByCompany(companyId);
+            try
+            {
+                List<Project> projects = await _context.Projects.Where(p => p.CompanyId == companyId && p.Archived == true)
+                                            .Include(p => p.Members)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.Comments)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.Attachments)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.History)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.DeveloperUser)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.OwnerUser)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.Notifications)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketStatus)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketPriority)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketType)
+                                            .Include(p => p.ProjectPriority)
+                                            .ToListAsync();
 
-            return projects.Where(p => p.Archived == true).ToList();
+                return projects;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public Task<List<BTUser>> GetDevelopersOnProjectAsync(int projectId)
