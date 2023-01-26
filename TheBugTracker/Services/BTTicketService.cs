6149a0a31dbdcdab1d564220f1d899a64e46557f
AdmerDev <sjkrantz@aol.com>
2023-01-26 17:04:49 -0600

    Updated TIcketsController, BTTicketService, and _Layout. Created AssignDeveloperViewModel and UnassignedTickets view.

--- TheBugTracker/Services/BTTicketService.cs
+++ TheBugTracker/Services/BTTicketService.cs
@@ -410,6 +410,21 @@ namespace TheBugTracker.Services
             }
         }
 
+        public async Task<List<Ticket>> GetUnassignedTicketsAsync(int companyId)
+        {
+            List<Ticket> tickets = new();
+
+            try
+            {
+                tickets = (await GetAllTicketsByCompanyAsync(companyId)).Where(t => string.IsNullOrEmpty(t.DeveloperUserId)).ToList();
+                return tickets;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
         public async Task<int?> LookupTicketPriorityIdAsync(string priorityName)
         {
             try
@@ -461,5 +476,6 @@ namespace TheBugTracker.Services
                 throw;
             }
         }
+
     }
 }
