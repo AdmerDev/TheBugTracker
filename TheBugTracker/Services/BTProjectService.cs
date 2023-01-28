dee28d419cb4d1e3e74e5e3c96acc132c7535f4c
AdmerDev <sjkrantz@aol.com>
2023-01-28 17:30:33 -0600

    Updated ProjectsController, BTProjectService, IBTProjectService, _Layout, and _LoginPartial. Created AssignPMViewModel, AssignPM, and UnassignedProjects.

--- TheBugTracker/Services/BTProjectService.cs
+++ TheBugTracker/Services/BTProjectService.cs
@@ -50,7 +50,7 @@ namespace TheBugTracker.Services
             // Add the new project manager.
             try
             {
-                await AddProjectManagerAsync(userId, projectId);
+                await AddUserToProjectAsync(userId, projectId);
                 return true;
             }
             catch (Exception ex)
@@ -319,6 +319,34 @@ namespace TheBugTracker.Services
                 Console.WriteLine($"*** ERROR *** - Error Getting user projects list.   ---> {ex.Message}");
                 throw;
             }
+        }
+
+        public async Task<List<Project>> GetUnassignedProjectsAsync(int companyId)
+        {
+            List<Project> result = new();
+            List<Project> projects = new();
+
+            try
+            {
+                projects = await _context.Projects
+                                         .Include(p => p.ProjectPriority)
+                                         .Where(p => p.CompanyId == companyId).ToListAsync();
+
+                foreach (Project project in projects)
+                {
+                    if((await GetProjectMembersByRoleAsync(project.Id, nameof(Roles.ProjectManager))).Count == 0)
+                    {
+                        result.Add(project);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return result;
         }
 
         public async Task<List<BTUser>> GetUsersNotOnProjectAsync(int projectId, int companyId)
